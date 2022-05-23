d177965fd0384777e5e377739dc85df140ed8688
frlyh <liyaohua_it@163.com>
2022-05-23 11:35:37 +0800

    初始化

---
+++ src/main/java/com/frlyh/exa/ExaApplication.java
@@ -0,0 +1,13 @@
+package com.frlyh.exa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaApplication.class, args);
+    }
+
+}
